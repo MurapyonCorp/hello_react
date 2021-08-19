7dd0931cc22d01e47e85d3cb75fd0574e8b921e3
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-08-19 09:20:14 +0900

    thisの中身 1.new演算子

--- Riakuto-StartingReact-ja3.1-master/02-javascript/08-this/call-this.js
+++ Riakuto-StartingReact-ja3.1-master/02-javascript/08-this/call-this.js
@@ -6,3 +6,10 @@ const Person = function (name) {
 
 const kanae = Person.call({ gender: 'f' }, 'Kanae');
 console.log(kanae);
+
+// new演算子をつけて呼び出したとき:新規生成されるオブジェクト
+const dump = function() { console.log('`this` is', this); };
+const obj = new dump();
+console.log(obj)
+console.log(dump.prototype)
+console.log(obj !== dump.prototype)
