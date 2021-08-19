20b77d7159987ca7feca7a22944019dcdc5b0da4
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-08-19 09:28:55 +0900

    thisの中身　2.メソッドとして実行

--- Riakuto-StartingReact-ja3.1-master/02-javascript/08-this/call-this.js
+++ Riakuto-StartingReact-ja3.1-master/02-javascript/08-this/call-this.js
@@ -7,9 +7,18 @@ const Person = function (name) {
 const kanae = Person.call({ gender: 'f' }, 'Kanae');
 console.log(kanae);
 
-// new演算子をつけて呼び出したとき:新規生成されるオブジェクト
+// (1) new演算子をつけて呼び出したとき : 新規生成されるオブジェクト
 const dump = function() { console.log('`this` is', this); };
 const obj = new dump();
 console.log(obj)
 console.log(dump.prototype)
 console.log(obj !== dump.prototype)
+
+// (2) メソッドとして実行されたとき : その所属するオブジェクト
+const foo = {
+  name: 'Foo Object',
+  dump() {
+    console.log(this);
+  },
+};
+foo.dump();
