65277827aea2728490ece61a9a5cfdb7bbdd3330
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-09-06 08:23:05 +0900

    reduce解説

--- Riakuto-StartingReact-ja3.1-master/03-fp/02-collection/array-iterate2.js
+++ Riakuto-StartingReact-ja3.1-master/03-fp/02-collection/array-iterate2.js
@@ -1,4 +1,6 @@
 const arr = [1, 2, 3, 4, 5];
 
 console.log(arr.reduce((n, m) => n + m));
+// reduceは引数に着目してみる。第2引数mにはarr要素の値が順番に代入される。第１引数nには前回の関数の実行結果が入ってくる。
+// そうして最終的にreduce()は最後に実行された値のみ返す。
 console.log(arr.sort((n, m) => n > m ? -1 : 1));
