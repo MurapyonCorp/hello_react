8992791293927fd831ecbcbf3d60c3f44b488ef4
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-09-09 09:12:37 +0900

    コメント解説

--- Riakuto-StartingReact-ja3.1-master/03-fp/03-first-class/open-counter.js
+++ Riakuto-StartingReact-ja3.1-master/03-fp/03-first-class/open-counter.js
@@ -1,5 +1,8 @@
-let count = 0;
+// グローバル変数はどこからでも任意に書き換えができてしまう。そのため丸ごと関数の中に入れてしまう。
+const counter = () => {
+  let count = 0;
 
-const increment = () => {
-  return count += 1;
+  const increment = () => {
+    return count += 1;
+  };
 };
