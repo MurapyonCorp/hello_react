fca095cce44e5204faabda49a496ce87051cb999
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-09-30 09:40:25 +0900

    comment resolve

--- Riakuto-StartingReact-ja3.1-master/04-typescript/04-type-composition/non-null.ts
+++ Riakuto-StartingReact-ja3.1-master/04-typescript/04-type-composition/non-null.ts
@@ -4,7 +4,15 @@ type Resident = {
   mom?: Resident;
 };
 
-const getMomName = (resident: Resident): string => resident.mom.lastName;
+/*
+Resident型のmomプロパティは省略可能であるため、undefinedへのアクセスになる可能性がある。
+コンパイルするまでもなくVSCodeが教えてくれる。
+*/
+const getMomName = (resident: Resident): string => resident.mom!.lastName;
+/*
+mom!.lastNameの！は非Nullアサーション演算子といい、
+「ここには絶対にnullもundefinedも入らないよ」とコンパイラを黙らせている。
+*/
 
 const patty = { familyName: 'Hope-Rabbit', lastName: 'patty' };
 console.log(getMomName(patty));
