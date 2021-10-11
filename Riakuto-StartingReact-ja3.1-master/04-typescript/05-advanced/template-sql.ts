78bc075ab59779532c967e3dc3ad00540ce68b29
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-10-11 09:26:23 +0900

    comment resolve

--- Riakuto-StartingReact-ja3.1-master/04-typescript/05-advanced/template-sql.ts
+++ Riakuto-StartingReact-ja3.1-master/04-typescript/05-advanced/template-sql.ts
@@ -2,6 +2,7 @@ const tables = ['users', 'posts', 'comments'] as const;
 type Table = typeof tables[number];
 type AllSelect = `SELECT * FROM ${Table}`;
 type LimitSelect = `${AllSelect} LIMIT ${number}`;
+// 文字列でべた書きしていたクエリやパスなんかにも型を付けられるようになった。
 
 const createQuery = (table: Table, limit?: number): AllSelect | LimitSelect =>
   limit ? `SELECT * FROM ${table} LIMIT ${limit}` as const
