6f088acc5cdb61e61287edbcee877f28832869ac
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-10-11 09:25:37 +0900

    comment resolve

--- Riakuto-StartingReact-ja3.1-master/04-typescript/05-advanced/greet.java
+++ Riakuto-StartingReact-ja3.1-master/04-typescript/05-advanced/greet.java
@@ -4,6 +4,10 @@ public class Main {
     greet("Usagi");
   }
 
+  /*
+  メソッドのオーバーロード
+  => 同じメソッド名であるが、引数の型や個数による与え方で異なるメソッドが実行される。
+  */
   static void greet() {
     System.out.println("Hello!");
   }
