d1a2ce9cb6a80ad51782299898dad606ad880923
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-10-11 09:26:04 +0900

    comment resolve

--- Riakuto-StartingReact-ja3.1-master/04-typescript/05-advanced/overload-statement.ts
+++ Riakuto-StartingReact-ja3.1-master/04-typescript/05-advanced/overload-statement.ts
@@ -18,7 +18,9 @@ class CrisisCompact implements Compact {
 
 function transform(): void;
 function transform(item: Brooch): void;
+// 引数の型の指定はクラスだけでなくimplements元のオブジェクトの型(インターフェース)でも指定可能
 function transform(item: Compact): void;
+// 引数として与えた変身アイテムによって返信パターンが変化する。
 function transform(item?: Brooch | Compact): void {
   if (item instanceof Brooch) {
     console.log('Moon crystal power💎, make up!!');
@@ -31,6 +33,12 @@ function transform(item?: Brooch | Compact): void {
   } else {
     console.log('Item is fake...😖');
   }
+  /*
+  構造的サブタイピング(Structural Subtyping)
+  ---> 型の構造さえ合致していれば型チェックをパスする。またその型を拡張したサブタイプも型の比較では真となる。
+  今回の例で示すと
+  pentagram: stringさえ備えていればBrooch型にイコール、silverCrystal: booleanプロパティさえ備えていればCompact型にイコールとコンパイラに判断される。
+  */
 }
 
 transform();
